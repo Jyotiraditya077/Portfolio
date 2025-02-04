ed79bebffda8b8ac0b05ef80d2634b272b09a7d1
jyotiraditya swain <jyotiradityaswain123@gmail.com>
2025-02-04 22:05:40 +0530

    footer update

--- src/App.tsx
+++ src/App.tsx
@@ -334,7 +334,7 @@ function App() {
       <footer className="border-t border-white/10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <p className="text-center text-gray-400">
-            {new Date().getFullYear()} Jyotiraditya Swain. All rights reserved.
+            © {new Date().getFullYear()} Jyotiraditya Swain. All rights reserved.
           </p>
         </div>
       </footer>
